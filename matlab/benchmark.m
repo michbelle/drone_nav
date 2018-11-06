afcaf4584cd2c354f398630ef7f8ff1f2a90a3cd
michbelle <40576383+michbelle@users.noreply.github.com>
2018-11-06 17:23:11 +0100

    Update benchmark.m

--- matlab/benchmark.m
+++ matlab/benchmark.m
@@ -2,17 +2,17 @@
 %Acquisisco i dati del path calcolato da MoveIt
 
 %rosinit;
-orientation = rossubscriber('/move_group/display_planned_path');
-si=size(orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points);
+%stepss = rossubscriber('/move_group/result');
+si=size(stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points);
 si=si(1);
 for x=1:si
-    X = orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points(x).Transforms.Translation.X;
-    Y = orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points(x).Transforms.Translation.Y;
-    Z = orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points(x).Transforms.Translation.Z;
-    QW = orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.W;
-    QX = orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.X;
-    QY = orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.Y;
-    QZ = orientation.LatestMessage.Trajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.Z;
+    X = stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points(x).Transforms.Translation.X;
+    Y = stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points(x).Transforms.Translation.Y;
+    Z = stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points(x).Transforms.Translation.Z;
+    QW = stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.W;
+    QX = stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.X;
+    QY = stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.Y;
+    QZ = stepss.LatestMessage.Result.PlannedTrajectory.MultiDofJointTrajectory.Points(x).Transforms.Rotation.Z;
     QWW=[QWW;QW];
     QXX=[QXX;QX];
     QYY=[QYY;QY];
@@ -20,7 +20,6 @@ for x=1:si
 
     A=[X;Y;Z];
 	ori=[ori,A];
-	x=x+1;
 end
 %ori
 Q=quaternion(QWW,QXX,QYY,QZZ);
@@ -36,3 +35,4 @@ for c=2:si
 end
 
 lunghezza_tot
+time=stepss.LatestMessage.Result.PlanningTime
