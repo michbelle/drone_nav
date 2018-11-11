a324010b92780417a5443e4a4d00ff29be321322
michbelle <40576383+michbelle@users.noreply.github.com>
2018-11-11 19:13:03 +0100

    Update run_before.m

--- matlab/run_before.m
+++ matlab/run_before.m
@@ -2,4 +2,8 @@
 % Si lancia per iniziare le variabili
 % si lancia questo file, poi gli altri
 rosinit;
-stepss = rossubscriber('/move_group/result');
+%stepss = rossubscriber('/move_group/result');
+%orientation = rossubscriber('/move_group/display_planned_path');
+startposition = rossubscriber('/mavros/global_position/global');
+endpoint = rossubscriber('/mavros/local_position/pose');
+%c=fopen('risult', 'w')
