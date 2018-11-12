8dd0133d9693d38edf1edf7ea6b9e7ebcafa228f
michbelle <40576383+michbelle@users.noreply.github.com>
2018-11-12 19:44:08 +0100

    Update risultatoer.m

--- matlab/risultatoer.m
+++ matlab/risultatoer.m
@@ -7,6 +7,6 @@ fprintf('errore rispetto a x = %3.2f\n', d(1,1));
 fprintf('errore rispetto a y = %3.2f\n', d(2,1));
 fprintf('errore rispetto a z = %3.2f\n', d(3,1));
 
-ertot=sqrt(d(3,1)^2+(sqrt(d(1,1)^2)+d(2,1)^2)^2);
+ertot=sqrt(d(3,1)^2+(sqrt(d(1,1)^2+d(2,1)^2)^2);
 
 fprintf('errore TOTALE = %5.2f\n',ertot);
